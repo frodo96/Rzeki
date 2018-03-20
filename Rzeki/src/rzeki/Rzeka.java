3259ee2d2a5b2b620bfc01a2b67167f73d769121
Student <Student@193.107.34.217>
2018-03-20 13:56:35 +0100

    Rzeki

---
+++ Rzeki/src/rzeki/Rzeka.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rzeki;
+
+/**
+ *
+ * @author Student
+ */
+public class Rzeka {
+    private String nazwa;
+    private int dlugosc;
+    
+    public Rzeka(String nazwa, int dlugosc)
+    {
+        this.nazwa=nazwa;
+        this.dlugosc=dlugosc;
+    }
+    
+    public  String toString()
+    {
+        return "Rzeka{" + " nazwa= " + nazwa + "," + " dlugosc= " + dlugosc;         
+    }
+    
+    public boolean equals(Rzeka r)
+    {
+        return nazwa.equals(r.nazwa);
+    }
+    
+    public int getDlugosc()
+    {
+        return dlugosc;
+    }
+}
